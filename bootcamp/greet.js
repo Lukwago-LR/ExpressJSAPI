2258629f35a29869c301eac2941816ddb95909c1
Lukwago-LR <lukwagorsaved@gmail.com>
2023-09-20 10:36:05 +0200

    Lord my Saviour

--- bootcamp/greet.js
+++
@@ -1,3 +0,0 @@
-export default function greet(nameUser) {
-    return nameUser;
-}
--- bootcamp/wordLengths.js
+++
@@ -1,4 +0,0 @@
-export default function wordLengths(sentence) {
-    let strL = sentence.split(' ');
-    return strL.length;
-}
